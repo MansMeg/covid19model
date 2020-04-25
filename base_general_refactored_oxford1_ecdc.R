cc062c8b206210fbfa6b2b5dfc2d774015d710f3
Måns Magnusson <mons.magnusson@gmail.com>
2020-04-25 08:59:18 +0300

    Increas max treedepth

--- base_general_refactored_oxford1_ecdc.R
+++ base_general_refactored_oxford1_ecdc.R
@@ -138,7 +138,7 @@ m = stan_model(paste0('stan-models/',StanModel,'.stan'))
 if(DEBUG) {
   fit = sampling(m,data=stan_data,iter=40, warmup=20, chains=2, seed = 4711)
 } else if (FULL) {
-  fit = sampling(m,data=stan_data,iter=4000,warmup=2000,chains=4,thin=4,control = list(adapt_delta = 0.95, max_treedepth = 10))
+  fit = sampling(m,data=stan_data,iter=4000,warmup=2000,chains=4,thin=4,control = list(adapt_delta = 0.95, max_treedepth = 15))
 } else { 
   fit = sampling(m,data=stan_data,iter=200,warmup=100,chains=4,thin=4,control = list(adapt_delta = 0.95, max_treedepth = 10))
 }  
