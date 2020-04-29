04ffa27f297f1a8ce89647bad996e3f12862f875
Måns Magnusson <mons.magnusson@gmail.com>
2020-04-29 20:08:07 +0300

    Add alpha prior model changes

---
+++ triton/run/covid19_model1an_base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=0-08:00:00
+#SBATCH --cpus-per-task=4
+
+# Defaults to directory of when submitted.  Explicit cd if needed (slurm defaults
+# to submission directory).
+# cd $WRKDIR/myproject/
+
+# output goes into hello.out
+# If you use srun for each command, the mem/cpu usage of each step
+# can be seen individually with "slurm history"
+module load r/3.6.1-python3
+export PBS_JOBID="model1_base"
+Rscript base_model1_base.R base_general_speed_alpha --full
+
