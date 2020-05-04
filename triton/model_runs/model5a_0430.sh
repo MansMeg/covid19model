99fd04b5a4917b0b0e5790259c6d9e7f29b32858
Måns Magnusson <mons.magnusson@gmail.com>
2020-05-04 13:15:29 +0300

    Add triton models

---
+++ triton/model_runs/model5a_0430.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --time=0-04:00:00
+#SBATCH --cpus-per-task=4
+
+date
+
+export JOB_ID="model5a_0430"
+
+module load r/3.6.1-python3
+
+# Setup yml config file
+cat > $JOB_ID.yml <<EOF
+
+job_id: "$JOB_ID"
+
+libraries:
+  - splines
+
+model_arguments:
+  stan_model: "base_general_speed_alpha.stan"
+  model_formula: "~ transit_stations"
+  N2: 110
+  seed: 4711
+  date:
+    start: "2020-01-01"
+    end: "2020-04-30"
+
+data:
+  countries: ["Denmark", "Italy", "Germany", "Spain", "United_Kingdom", "France", "Norway", "Belgium", "Austria", "Sweden", "Switzerland", "Greece", "Portugal", "Netherlands", "Finland"]
+  daily_data: "odv5g"
+
+stan_arguments:
+  iter: 4000
+  warmup: 2000
+  chains: 4
+  thin: 4
+  control:
+    adapt_delta: 0.95
+    max_treedepth: 10
+
+EOF
+
+# Run model
+Rscript run_model.R $JOB_ID.yml
+
+# Remove config
+rm $JOB_ID.yml
+
+date
+
