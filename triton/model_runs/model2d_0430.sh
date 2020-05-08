8b9f08fd18d79ac813e3e2dc49f5cd39c72e8322
Måns Magnusson <mons.magnusson@gmail.com>
2020-05-08 12:02:08 +0300

    Added final models

---
+++ triton/model_runs/model2d_0430.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --time=0-12:00:00
+#SBATCH --cpus-per-task=4
+
+date
+
+git rev-parse --short HEAD
+
+export JOB_ID="model2d_0430"
+
+module load r/3.6.1-python3
+
+# Setup yml config file
+cat > $JOB_ID.yml <<EOF
+
+job_id: "$JOB_ID"
+
+libraries:
+  - splines
+
+model_arguments:
+  stan_model: "base_general.stan"
+  model_formula: "~ neg_log_transit_proportion + StringencyIndex"
+  N2: 110
+  seed: 4711
+  date:
+    start: "2020-01-01"
+    end: "2020-04-30"
+
+data:
+  countries: ["Denmark", "Italy", "Germany", "Spain", "United_Kingdom", "France", "Norway", "Belgium", "Austria", "Sweden", "Switzerland", "Greece", "Portugal", "Netherlands", "Finland"]
+  daily_data: "odv5g"
+
+stan_arguments:
+  iter: 2500
+  warmup: 2000
+  chains: 4
+  thin: 1
+  control:
+    adapt_delta: 0.98
+    max_treedepth: 10
+
+EOF
+
+# Run model
+Rscript run_model.R $JOB_ID.yml
+
+# Remove config
+rm $JOB_ID.yml
+
+date
+
