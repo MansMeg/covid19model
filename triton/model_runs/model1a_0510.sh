8359396a72a2d6d1b6c0140b21bf68b6c0678f3a
Måns Magnusson <mons.magnusson@gmail.com>
2020-05-14 21:12:00 +0300

    Init models

---
+++ triton/model_runs/model1a_0510.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --time=0-12:00:00
+#SBATCH --cpus-per-task=4
+
+date
+
+git rev-parse --short HEAD
+
+export JOB_ID="model1a_0510"
+
+module load r/3.6.1-python3
+
+# Setup yml config file
+cat > $JOB_ID.yml <<EOF
+
+job_id: "$JOB_ID"
+
+model_arguments:
+  stan_model: "base_general.stan"
+  model_formula: "~ schools...universities + self.isolating.if.ill + public.events + any.intervention + lockdown + social.distancing.encouraged"
+  N2: 120
+  seed: 4711
+  date:
+    start: "2020-01-01"
+    end: "2020-05-10"
+
+data:
+  countries: ["Denmark", "Italy", "Germany", "Spain", "United_Kingdom", "France", "Norway", "Belgium", "Austria", "Sweden", "Switzerland", "Greece", "Portugal", "Netherlands", "Finland"]
+  daily_data: "icv3_0514"
+
+stan_arguments:
+  iter: 3000
+  warmup: 2000
+  chains: 4
+  thin: 2
+  control:
+    adapt_delta: 0.99
+    max_treedepth: 15
+
+EOF
+
+# Run model
+Rscript run_model.R $JOB_ID.yml
+
+# Remove config
+rm $JOB_ID.yml
+
+date
---
+++ triton/model_runs/model2a_0510.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --time=0-04:00:00
+#SBATCH --cpus-per-task=4
+
+date
+
+git rev-parse --short HEAD
+
+export JOB_ID="model2a_0510"
+
+module load r/3.6.1-python3
+
+# Setup yml config file
+cat > $JOB_ID.yml <<EOF
+
+job_id: "$JOB_ID"
+
+libraries:
+  - splines
+
+model_arguments:
+  stan_model: "base_hiearchical2.stan"
+  model_formula: "~ C1b + C2b + C3b + C4b + C5b + C6b + H2b + neg_log_transit_proportion"
+  N2: 120
+  seed: 4711
+  date:
+    start: "2020-01-01"
+    end: "2020-05-10"
+
+data:
+  countries: ["Denmark", "Italy", "Germany", "Spain", "United_Kingdom", "France", "Norway", "Belgium", "Austria", "Sweden", "Switzerland", "Greece", "Portugal", "Netherlands", "Finland"]
+  daily_data: "odv5g_0514"
+
+stan_arguments:
+  iter: 3000
+  warmup: 2000
+  chains: 4
+  thin: 2
+  control:
+    adapt_delta: 0.95
+    max_treedepth: 10
+
+EOF
+
+# Run model
+Rscript run_model.R $JOB_ID.yml
+
+# Remove config
+rm $JOB_ID.yml
+
+date
+
